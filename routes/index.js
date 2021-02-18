5b3cde10c732610caf82e72662d5739d498b2198
Mike McDonald <McDonam6@oregonstate.edu>
2021-02-17 21:38:01 -0800

    Re-factored for Azure deployment, Style fix
    Refactored layouts and file structure, eliminated unnecessary node modules, prepped/tested deployment on Azure and fixed the Stylesheet reference issue

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+// Get home page
+router.get('/', function(req, res) {
+  res.render('home');
+});
+
+// Get Home page with route
+router.get('/Home', function(req, res) {
+  res.render('home');
+});
+
+// Get About me page
+router.get('/AboutMe', function(req,res){
+  res.render('aboutMe');
+});
+
+// Get Experience Page
+router.get('/Experience', function(req, res){
+  res.render('experience');
+})
+
+// Get Projects Page
+router.get('/Projects', function(req, res){
+  res.render('projects');
+})
+
+// Get Contact info Page
+router.get('/Contact', function(req, res){
+  res.render('contact');
+})
+
+module.exports = router;
